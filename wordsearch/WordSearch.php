3416ae140e22d209ad66796b21d1895d7769518d
lbarrio <lbarrio@digbang.com>
2021-06-23 15:39:58 -0300

    devChallenge | WordSearch Answer [Zoe]

--- wordsearch/WordSearch.php
+++ wordsearch/WordSearch.php
@@ -10,20 +10,50 @@ $board = [
     ['o', 'f', 'z', 's']
 ];
 
-getPosInBoard('l',$board) retornará [1,2] >> O sea, fila 1, columna 2 
+getPosInBoard('l',$board) retornará [1,2] >> O sea, fila 1, columna 2
 */
 
 function getPosInBoard($letter,$board) {
-	for ($i=0; $i < count($board) ; $i++) { 
-		for ($j=0; $j < count($board[$i]) ; $j++) { 
-			if($letter == $board[$i][$j]) {
-				return array($i,$j);
-			}
-		}
-	}
-	return NULL;
+    for ($i=0; $i < count($board) ; $i++) {
+        for ($j=0; $j < count($board[$i]) ; $j++) {
+            if($letter == $board[$i][$j]) {
+                return array($i,$j);
+            }
+        }
+    }
+    return NULL;
 }
 
-// Debe retornar true o false
-function searchWord($board, $str) {
+function searchWord($board, $str): bool
+{
+    $posB = getPosInBoard($str[0], $board);
+
+    if($posB == NULL){
+        return false;
+    }
+
+    for ($i=1; $i<strlen($str); $i++) {
+        $pos = getPosInBoard($str[$i], $board);
+
+        if ($pos === NULL) {
+            return false;
+        }
+
+        if(! ady($posB, $pos)) {
+            return false;
+        }
+
+        $posB = $pos;
+    }
+
+    return true;
+}
+
+function ady($a, $b): bool
+{
+    if(abs($a[0]-$b[0]) == 1 && abs($a[1]-$b[1]) == 0) return true;
+
+    if(abs($a[0]-$b[0]) == 0 && abs($a[1]-$b[1]) == 1) return true;
+
+    return false;
 }
